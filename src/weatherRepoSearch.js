27a43be3e30cce9b29ed3889663aea032470c9fa
sarathchandrav <58840794+sarathchandrav@users.noreply.github.com>
2020-04-20 16:53:44 +0530

    test deploy

--- src/weatherRepoSearch.js
+++ src/weatherRepoSearch.js
@@ -1,44 +1,10 @@
-// import React from 'react';
-// import {Link} from 'react-router-dom';
-
-
-// class SearchBar extends React.Component{
-
-//     state={SearchValue:''};
-
-
-//     onFormSubmit(event) {
-        
-//         event.preventDefault();
-//        // WeatherReport(this.state.SearchValue);
-//     }
-
-//     render(){
-//         return(
-//             <form onSubmit={(event) => this.onFormSubmit(event)} style={{ float: 'right', position: 'relative' }}>
-//                     <i className="search icon"></i>
-//                     <input
-//                         value={this.state.SearchValue}
-//                         onChange={(e) => this.setState({ SearchValue: e.target.value })} placeholder='Enter The City' type='text' name='name' style={{ width: 'auto' }} />
-//                     <button onClick={(event) => this.onFormSubmit(event)} style={{ margin: '10px', width: 'auto' }}> <Link to="/WeatherReport/Search">Search</Link> </button>
-
-//                 </form>
-//         );
-        
-//     }
-// }
-
-// export default SearchBar;
-
-
-
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 //import {connect} from 'react-redux';
 //import { fetchSearchCityAPI } from './actions/index';
 
 import { Link } from 'react-router-dom';
-class weatherRepoSearch extends React.Component {
+class WeatherRepoSearch extends React.Component {
     renderInput = (searchProps) => {
         const search =  '/WeatherReport/Search/'+searchProps.input.value;
         return (<div><input {...searchProps.input} /><button><Link to={search}>search</Link></button></div>);
@@ -62,6 +28,6 @@ class weatherRepoSearch extends React.Component {
 
 
 
-export default reduxForm({form: 'weatherRepoForm'})(weatherRepoSearch);
+export default reduxForm({form: 'weatherRepoForm'})(WeatherRepoSearch);
 
 //export default connect( mapStateToProps , {fetchSearchCityAPI})(formWrapped)
